5da0121413ef94d653613c98eef6444d82d4047f
parastoopourazizi <napourazizi@gmail.com>
2023-10-17 13:21:55 +0330

    add css

--- src/Calculator.js
+++ src/Calculator.js
@@ -6,24 +6,24 @@ function Calculator(){
                 
             </div>
             <div className="buttons">
-                <button>clear</button>
-                <button>C</button>
-                <button>÷</button>
+                <button className='color twocol'>clear</button>
+                <button className='color'>C</button>
+                <button className='color'>÷</button>
                 <button>7</button>
                 <button>8</button>
                 <button>9</button>
-                <button>×</button>
+                <button className='color'>×</button>
                 <button>4</button>
                 <button>5</button>
                 <button>6</button>
-                <button>-</button>
+                <button className='color'>-</button>
                 <button>1</button>
                 <button>2</button>
                 <button>3</button>
-                <button>+</button>
+                <button className='color'>+</button>
                 <button>0</button>
                 <button>.</button>
-                <button>=</button>
+                <button className='color twocol'>=</button>
                 
                 
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'
 
 
 
@@ -8,8 +10,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
 root.render(
+    <>
+     <App />
+    </>
   
- <h1>jdjj</h1>
+
 
 
  
